e410c9e6e62a3b5d60ce18622e697846229828ac
agustn134 <agustinparralopez1@gmail.com>
2026-07-14 13:39:25 -0600

    Initial commit

---
+++ wear/src/main/java/com/alp/utngrunner/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.alp.utngrunner.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun UTNGRunnerTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
